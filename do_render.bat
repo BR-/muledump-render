b0ee777ba4dee40c78a30ad01ceb570b2bc8c5c1
BR <wishingwellwellwishing@hotmail.com>
2023-07-11 13:57:19 -0700

    explicity run python instead of relying on filetype

--- do_render.bat
+++ do_render.bat
@@ -3,7 +3,7 @@
 git status
 
 del http_cache.sqlite constants.js sheets.js renders.png
-pipenv run render.py
+pipenv run python render.py
 copy /Y constants.js ..\muledump\lib\
 copy /Y sheets.js ..\muledump\lib\
 copy /Y renders.png ..\muledump\lib\
